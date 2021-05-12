a90e36b36c3d443d92d0f40b9a4d3f6b851728b1
Хайрбек Мамбетов <mnocard@gmail.com>
2021-05-12 14:22:40 +0500

    Добавляем проект клиента службы

---
+++ WcfServiceClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

105b04ad7348b587881cad08dffa0b73f3094eac
Хайрбек Мамбетов <mnocard@gmail.com>
2021-05-12 14:06:22 +0500

    Добавьте файлы проекта.

---
+++ TestWcfServiceLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения о сборке можно задать с помощью следующего 
+// атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+// связанные со сборкой.
+[assembly: AssemblyTitle("TestWcfServiceLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("*")]
+[assembly: AssemblyProduct("TestWcfServiceLibrary")]
+[assembly: AssemblyCopyright("Copyright © * 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Если для ComVisible установить значение false, типы в этой сборке не будут поддерживаться 
+// COM-компонентами.  При необходимости доступа к какому-либо типу в этой сборке 
+// модели COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если данный проект видим для COM
+[assembly: Guid("9a2e2b08-f057-444c-9405-2f672f097ea1")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestWfrServiceConsoleNF/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using TestWcfServiceLibrary;
+
+namespace TestWfrServiceConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8733/Design_Time_Addresses/TestWcfServiceLibrary/calc/");
+
+            ServiceHost selfHost = new ServiceHost(typeof(CalculatorService), baseAddress);
+
+            try
+            {
+                selfHost.AddServiceEndpoint(typeof(ICalculatorService), new WSHttpBinding(), "CalculatorService");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                selfHost.Open();
+                Console.WriteLine("The service is ready.");
+
+                Console.WriteLine("Press <Enter> to terminate the service.");
+                Console.ReadKey();
+                selfHost.Close();
+            }
+            catch (CommunicationException ex)
+            {
+                Console.WriteLine("An exception occurred: {0}", ex.Message);
+                Console.ReadKey();
+                selfHost.Abort();
+            }
+        }
+    }
+}
